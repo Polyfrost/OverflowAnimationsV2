976e546543642db9256d3611c2296294785b6969
lego3708 <l3gomindstorms@gmail.com>
2021-01-21 08:56:02 +0000

    Add health bar flashing.

--- src/main/java/club/sk1er/oldanimations/mixins/GuiIngameForgeMixin.java
+++ src/main/java/club/sk1er/oldanimations/mixins/GuiIngameForgeMixin.java
@@ -3,8 +3,10 @@ package club.sk1er.oldanimations.mixins;
 import club.sk1er.oldanimations.config.OldAnimationsSettings;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraftforge.client.GuiIngameForge;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GuiIngameForge.class)
@@ -18,6 +20,9 @@ public class GuiIngameForgeMixin {
     private int removeShadow(FontRenderer fontRenderer, String text, int x, int y, int color) {
         return fontRenderer.drawString(text, x, y, color, OldAnimationsSettings.oldDebugScreen);
     }
-    
-    //TODO: Port health bar flash fix
+
+    @ModifyVariable(method = "renderHealth", at = @At(value = "LOAD", opcode = Opcodes.ILOAD, ordinal = 1), index = 5)
+    private boolean cancelFlash(boolean original) {
+        return original && !OldAnimationsSettings.oldHealth;
+    }
 }
