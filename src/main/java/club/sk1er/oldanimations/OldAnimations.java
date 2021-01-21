e0ec455214357aac8c18a34291aba604ae2d3f69
BuildTools <james.jenour@protonmail.com>
2021-01-21 14:01:36 +0800

    change forge version to '1.8.9-11.15.1.1722' to fix issue with ReplayMod ForgeGradle

--- src/main/java/club/sk1er/oldanimations/OldAnimations.java
+++ src/main/java/club/sk1er/oldanimations/OldAnimations.java
@@ -6,15 +6,13 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.modcore.api.ModCoreAPI;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(modid = OldAnimations.MODID, name = "Sk1er Old Animations", version = OldAnimations.VERSION)
 public class OldAnimations {
     public static final String MODID = "sk1er_old_animations";
     public static final String VERSION = "1.0";
     public static OldAnimationsSettings oldAnimationsSettings;
-    public static final Logger LOGGER = LogManager.getLogger("Sk1erOldAnimations");
+    //public static final Logger LOGGER = LogManager.getLogger("Sk1erOldAnimations");
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
