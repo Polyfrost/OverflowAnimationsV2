a03481b2438014cf2057df3e9ac2f9606585826f
LlamaLad7 <l3gomindstorms@gmail.com>
2021-08-04 23:54:32 +0100

    Fix: resolve incompatibility with RodColor.

--- src/main/java/club/sk1er/oldanimations/mixins/RenderFishMixin.java
+++ src/main/java/club/sk1er/oldanimations/mixins/RenderFishMixin.java
@@ -8,11 +8,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(RenderFish.class)
+@Mixin(value = RenderFish.class, priority = 1001)
 public class RenderFishMixin {
     @Redirect(method = "doRender", at = @At(value = "NEW", target = "net/minecraft/util/Vec3", ordinal = 0))
     private Vec3 oldFishingLine(double x, double y, double z) {
         return !OldAnimationsSettings.oldRod ? new Vec3(x, y, z) : FishingLineHandler.getInstance().getOffset();
-
     }
 }
