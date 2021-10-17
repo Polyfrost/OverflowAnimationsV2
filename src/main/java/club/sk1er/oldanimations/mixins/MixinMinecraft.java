6dc0d3e9d017909784677585c3ca6b20ce74d2e3
LlamaLad7 <l3gomindstorms@gmail.com>
2021-10-17 19:46:04 +0100

    Fix: resolve right-clicking cancelling destroying when there is no right click action.

--- src/main/java/club/sk1er/oldanimations/mixins/MixinMinecraft.java
+++ src/main/java/club/sk1er/oldanimations/mixins/MixinMinecraft.java
@@ -2,6 +2,7 @@ package club.sk1er.oldanimations.mixins;
 
 import club.sk1er.oldanimations.config.OldAnimationsSettings;
 import net.minecraft.client.Minecraft;
+import net.minecraft.item.EnumAction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +14,9 @@ public class MixinMinecraft {
     @Inject(method = "rightClickMouse", at = @At("HEAD"))
     public void rightClickMouse(CallbackInfo ci) {
         if (OldAnimationsSettings.punching &&
-            Minecraft.getMinecraft().playerController.getIsHittingBlock()) {
+                Minecraft.getMinecraft().playerController.getIsHittingBlock() &&
+                Minecraft.getMinecraft().thePlayer.getHeldItem() != null &&
+                Minecraft.getMinecraft().thePlayer.getHeldItem().getItemUseAction() != EnumAction.NONE) {
             // This sends packets to the server saying we stopped breaking.
             // Simply making getIsHittingBlock return false will make the server
             // think we are still breaking the block while right clicking.
