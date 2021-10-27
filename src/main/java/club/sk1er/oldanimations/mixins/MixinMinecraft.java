50081a60fbb2dc8282216018e1a0c063d0fac676
LlamaLad7 <l3gomindstorms@gmail.com>
2021-10-27 21:27:24 +0100

    Fix: Resolve block placing not taking priority over digging.

--- src/main/java/club/sk1er/oldanimations/mixins/MixinMinecraft.java
+++ src/main/java/club/sk1er/oldanimations/mixins/MixinMinecraft.java
@@ -3,6 +3,7 @@ package club.sk1er.oldanimations.mixins;
 import club.sk1er.oldanimations.config.OldAnimationsSettings;
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.EnumAction;
+import net.minecraft.item.ItemBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,7 +17,8 @@ public class MixinMinecraft {
         if (OldAnimationsSettings.punching &&
                 Minecraft.getMinecraft().playerController.getIsHittingBlock() &&
                 Minecraft.getMinecraft().thePlayer.getHeldItem() != null &&
-                Minecraft.getMinecraft().thePlayer.getHeldItem().getItemUseAction() != EnumAction.NONE) {
+                (Minecraft.getMinecraft().thePlayer.getHeldItem().getItemUseAction() != EnumAction.NONE ||
+                        Minecraft.getMinecraft().thePlayer.getHeldItem().getItem() instanceof ItemBlock)) {
             // This sends packets to the server saying we stopped breaking.
             // Simply making getIsHittingBlock return false will make the server
             // think we are still breaking the block while right clicking.
