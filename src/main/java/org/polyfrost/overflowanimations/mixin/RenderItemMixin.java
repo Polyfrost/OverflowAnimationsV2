b5151edc124b970278340230d800ba7167be569d
ImToggle <98242902+ImToggle@users.noreply.github.com>
2024-02-07 15:18:54 +0800

    small fix

--- src/main/java/org/polyfrost/overflowanimations/mixin/RenderItemMixin.java
+++ src/main/java/org/polyfrost/overflowanimations/mixin/RenderItemMixin.java
@@ -132,7 +132,7 @@ public class RenderItemMixin {
 
     @Redirect(method = "renderItemIntoGUI", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ItemModelMesher;getItemModel(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/client/resources/model/IBakedModel;"))
     public IBakedModel rodBowModelTexture(ItemModelMesher instance, ItemStack stack) {
-        if (OldAnimationsSettings.rodBowGuiFix && OldAnimationsSettings.INSTANCE.enabled) {
+        if (Minecraft.getMinecraft().thePlayer != null && OldAnimationsSettings.rodBowGuiFix && OldAnimationsSettings.INSTANCE.enabled) {
             EntityPlayer entityplayer = Minecraft.getMinecraft().thePlayer;
             Item item = stack.getItem();
             String inventory = "inventory";
