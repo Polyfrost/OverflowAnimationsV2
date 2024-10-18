d966b8d4afd8f5b47054e6494a83e01ffbed265d
Deftu <me@deftu.dev>
2024-10-19 01:29:13 +0200

    Fix command not working

--- src/main/java/org/polyfrost/overflowanimations/command/OldAnimationsCommand.java
+++ src/main/java/org/polyfrost/overflowanimations/command/OldAnimationsCommand.java
@@ -2,12 +2,14 @@ package org.polyfrost.overflowanimations.command;
 
 import org.polyfrost.oneconfig.api.commands.v1.factories.annotated.Command;
 import org.polyfrost.overflowanimations.config.OldAnimationsSettings;
+import org.polyfrost.utils.v1.dsl.ScreensKt;
 
 @Command(value = {"overflowanimations", "oam", "oldanimations", "animations"}, description = "Overflow Animations")
 public class OldAnimationsCommand {
     @Command
-    public void handle() {
+    public void main() {
         //todo: wyvest help
 //        OldAnimationsSettings.INSTANCE.openGui();
+        ScreensKt.openUI(OldAnimationsSettings.INSTANCE);
     }
 }
